9f274b6f8d08b8734c4df4968e751c4437208e2d
thuong-k99 <quangthuong923@gmail.com>
2025-03-20 01:22:13 +0700

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
-
-async def start(update: Update, context: CallbackContext):
-    await update.message.reply_text("Xin chào! Tôi là bot Telegram.")
-
-app = ApplicationBuilder().token("YOUR_BOT_TOKEN").build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
